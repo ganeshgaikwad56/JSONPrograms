5109ccf62a60c61f3bb48ac224c07c4e31907d69
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-04 15:11:35 +0530

    Adding All files for UC1

---
+++ FetchJsonForRice.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class FetchJsonForRice
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+                
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    var riceobject=  JsonConvert.DeserializeObject<Rice>(json);
+                    return riceobject;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+        }
+
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesofRice;
+        public List<TypesOfWheat> typesofWheat;
+        public List<TypesOfPulse> typesofPulse;
+    }
+
+
+    public class TypesOfRice
+    {
+        public string name;
+        public string weight;
+        public int Price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public string weight;
+        public int Price;
+    }
+    public class TypesOfPulse
+    {
+        public string name;
+        public string weight;
+        public int Price;
+    }
+}
